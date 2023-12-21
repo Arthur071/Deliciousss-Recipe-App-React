c51cb456f6770f9d323eaa3ceb21458451f9df2e
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-21 23:49:12 +0530

    fetching data from the api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,20 +11,15 @@ function Popular() {
   },[]);
 
   const getPopular = async () => {
-    const check = localStorage.getItem('popular');
-
-    if (check && check !=="undefined") {
-      setPopular(JSON.parse(check));
-    } else {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
 
       const data = await api.json();
-      // console.log(data);
-      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      console.log(data);
+      
 
       setPopular(data.recipes);
       console.log(data.recipes);
-    }
+    
   };
   return (
     <div>
