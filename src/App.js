7767db3cb4d554298103b77fafb98f3b422bd11f
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-22 23:06:28 +0530

    adding components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Pages from "./pages/Pages";
 import Catogaries from "./components/Catogaries";
+import {BrowserRouter} from 'react-router-dom';
+import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Search />
       <Catogaries />
       <Pages />
+      </BrowserRouter>
     </div>
   );
 }
