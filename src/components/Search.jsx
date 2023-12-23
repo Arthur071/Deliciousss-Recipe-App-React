0c555ce5ee9f2e64fd8c1fe0da7419dd27185229
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-23 15:11:23 +0530

    adding search query

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,27 @@
 import styled from "styled-components";
 import { useState } from "react";
 import {FaSearch} from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 
 
 function Search() {
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/' + input);
+        
+    }
+
   return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
         <div>
             <FaSearch></FaSearch>
-            <input type="text" />
+            <input onChange= {(e) => setInput(e.target.value)}type="text" value={input} />
         </div>
     </FormStyle>
-  )
-}
+  );
+} 
 
 const FormStyle = styled.form`
     margin: 0rem 20rem;
