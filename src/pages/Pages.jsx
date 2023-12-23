a454c33104bca5dd05c91dead1c5a0bef0f14f01
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-23 15:13:58 +0530

    adding more path to the routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,10 @@
+import React from "react";
 import Home from "./Home"
 import Cuisine from "./Cuisine";
 import {Route, Routes} from 'react-router-dom';
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+
 
 function Pages() {
   return (
@@ -8,6 +12,11 @@ function Pages() {
     <Routes>
       <Route path="/" element = {<Home />} />
       <Route path="/cuisine/:type" element = {<Cuisine />} />
+      <Route path="/searched/:search" element = {<Searched />} />
+      <Route path="/recipe/:name" element = {<Recipe />} />
+
+
+
     </Routes>
     
   )
